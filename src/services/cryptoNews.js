7088fbb37803252d6a9b3536ab205db9533be15d
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2023-08-02 02:39:05 +0000

    Crypto News Api integrated with RTK

---
+++ src/services/cryptoNews.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  "X-BingApis-SDK": "true",
+  "X-RapidAPI-Key": "5f39635c49msh1e63c99047a5d09p1d7526jsn44c760b940eb",
+  "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: baseUrl,
+  }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
